9678d174d7e9b1be6ff33f326460c90525884f77
Paco Menchaca <pacocoder86@outlook.com>
2022-06-23 01:53:45 -0500

    header/ok, footer/1/2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 import { EcomScreen } from "./pages/EcomScreen/EcomScreen.jsx";
-
+import Footer from "./pages/footer/Footer";
 
 const App = () => {
   return (
 
     <div className="maincontainer">
       <EcomScreen />
-
+      <Footer />
     </div>
 
   );
---
+++ src/pages/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.css";
+
+export default function Footer() {
+    return (
+        <div className="navbar is-primary-footer">
+            <div className="navbar-brand-footer">
+                <a className="aio" href="/">Facebook</a>
+                <a className="aho" href="/">Instagram</a>
+                <a className="ahor" href="/">Twitter</a>
+            </div>
+        </div>
+    );
+}
